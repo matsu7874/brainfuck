2d3cc05bfd44aee8d3d1527d6a2a64cd02e6de67
matsu7874 <mtsmtkmt@gmail.com>
2019-06-08 23:16:40 +0900

    使い方の説明を追加

---
+++ README.md
@@ -0,0 +1,10 @@
+# brainfuck
+
+Brainfuck interpreter written in Rust.
+
+## Usage
+
+```sh
+$ cargo build --release
+$ target/release/brainfuck sample_program/hello_world.bf
+```
--- src/main.rs
+++ src/main.rs
@@ -2,22 +2,24 @@ use brainfuck::{Interpreter, Lexer};
 use std::env;
 use std::fs::File;
 use std::io::Read;
-use std::io::BufReader;
-use std::io::Stdin;
 use std::process;
 
 fn main() {
     let args = env::args().collect::<Vec<String>>();
-    println!("{:?}", args);
     if args.len() <= 1 {
-        println!("");
+        println!(
+            "Brainfuck interpreter written in Rust.
+Usage:
+    brainfuck source.bf"
+        );
         return process::exit(64);
     }
 
     let mut p = String::new();
     let file_name = &args[1];
     let mut f = File::open(file_name).expect("file not found");
-    f.read_to_string(&mut p).expect("something went wrong reading the file");
+    f.read_to_string(&mut p)
+        .expect("something went wrong reading the file");
 
     let mut interpreter = Interpreter::new();
     let program = Lexer::lex(&p);
